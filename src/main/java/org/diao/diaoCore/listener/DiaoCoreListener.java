93f99ef4d4b66c98a3e6203eefa316f5bc0536b0
ZHI-CCC <549726031@qq.com>
2025-10-06 18:36:04 +0800

    修改了一些东西

--- src/main/java/org/diao/diaoCore/listener/DiaoCoreListener.java
+++ src/main/java/org/diao/diaoCore/listener/DiaoCoreListener.java
@@ -5,11 +5,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.diao.diaoCore.Main; // 仍然需要 Main 实例来获取 Logger
-import yslelf.cloudpick.bukkit.api.PacketSender; // 导入 CloudPick API 的 PacketSender
-import yslelf.cloudpick.bukkit.api.event.CustomPacketEvent; // 导入 CloudPick API 的 CustomPacketEvent
-
-import java.util.Arrays;
+import org.diao.diaoCore.Main;
+import yslelf.cloudpick.bukkit.api.PacketSender;
 
 /**
  * DiaoCore 插件的事件监听器。
@@ -41,16 +38,16 @@ public class DiaoCoreListener implements Listener {
             if (plugin.getServer().getPluginManager().getPlugin("CloudPick") != null && plugin.getServer().getPluginManager().getPlugin("CloudPick").isEnabled()) {
                 // 获取被击杀实体的显示名称
                 String entityName = killedEntity.getName();
-                // 获取击杀玩家的名称
-                String killerName = killer.getName();
+                // 获取被击杀实体的类型名称
+                String entityType = killedEntity.getType().name(); // 使用 .name() 获取枚举的字符串名称
 
                 // 直接通过 CloudPick API 发送数据给客户端
                 // 标识符: diaocore_kill
-                // 数据: 被击杀实体名称, 击杀玩家名称
-                PacketSender.sendCustomData(killer, "diaocore_kill", entityName, killerName);
+                // 数据: 被击杀实体名称, 被击杀实体类型名称
+                PacketSender.sendCustomData(killer, "diaocore_kill", entityName, entityType);
 
                 // 插件日志输出，方便调试
-                plugin.getLogger().info(String.format("玩家 %s 击杀了 %s, 已通过 diaocore_kill 发送至客户端。", killerName, entityName));
+                plugin.getLogger().info(String.format("玩家 %s 击杀了 %s (%s), 已通过 diaocore_kill 发送至客户端。", killer.getName(), entityName, entityType));
             } else {
                 plugin.getLogger().warning("尝试发送击杀信息到客户端，但 CloudPick 插件未启用或未安装！");
             }
